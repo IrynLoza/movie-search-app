b75b3b8bce7db53179fb3eaea3d36a04ea8d2012
Iryna Loza <irynloza@gmail.com>
2021-01-15 11:55:30 -0800

    Clean code

--- server.py
+++ server.py
@@ -9,63 +9,23 @@ app.secret_key = 'dev'
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 #The main endpoint
-
 @app.route('/')
 def root():
+    """Render homepage"""
+
     return render_template('index.html')
 
-#TODO 
-#Change to get
+
 @app.route('/api/movies', methods = ['POST'])
 def get_titles():
     """Get all titles by name"""
+
     data = request.data
     data_dict = json.loads(data)
     name = data_dict['name']
     page = data_dict['page']
-    print(f'data===>> {name}')
-    print(f'data===>> {page}')
     response_title = requests.get(f'http://www.omdbapi.com/?s={name}&apikey=fea3251d&page={page}')
     list_of_movies = response_title.json()
-    # total_results = int(response_title.json()['totalResults'])
-    # print(f'total_results===> {total_results}')
-
-    # full_list_of_movies = []
-
-    # for i in range(total_results//10):
-    #     response_title = requests.get(f'http://www.omdbapi.com/?s={name}&apikey=fea3251d&page={page}')
-    #     list_of_movies = response_title.json()
-    #     full_list_of_movies+= list_of_movies['Search']
-    #     page+= 1
-
-    # print(f'full_list_of_movies===> {full_list_of_movies}')
-    
-    # titles = []
-    # years = []
-   
-    # if total_results > 0:
-    #     pages = round(len(full_list_of_movies)// 10) 
-    #     for i in range(len(full_list_of_movies)):
-    #         title = full_list_of_movies[i]['Title']
-    #         print(f'title===> {title}')
-    #         print(f'i===> {i}')
-            
-    #         titles.append(title)
-    #         year = full_list_of_movies[i]['Year']
-    #         years.append(year) 
-
-
-    #         # for element in full_list_of_movies[i]['Search']:
-    #         #     print(f'element===> {element}')
-    #         # title = full_list_of_movies[i]['Search']
-    #         # print(f'element===> {element['Title']}')
-    
-    # else:
-    #     print('No such a movie')
-
-    # print(titles)        
-
-    # return jsonify({'status': 'ok', 'titles': titles, 'years': years, 'pages': pages})
 
     return jsonify({'status': 'ok', 'response': list_of_movies })
 
--- static/js/root.jsx
+++ static/js/root.jsx
@@ -15,8 +15,9 @@ function Search() {
     const [movies, setMovies] = React.useState([]);
     const [pages, setPages] = React.useState(0);
     const [nominates, setNominate] = React.useState([]);
-    const [currentPage, setCurrentPage] = React.useState(1)
+    const [currentPage, setCurrentPage] = React.useState(1);
 
+    //Handle search
     function search(value) {
         if (name) {
             fetch('/api/movies', {
@@ -24,12 +25,10 @@ function Search() {
                 body: JSON.stringify({ name, page: value }),
                 header: {
                     'Content-Type': 'application/json',
-                    // 'Accept': 'application/json'
                 },
             })
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data)
                     if (data.status === 'ok') {
                         //Add active key to object 
                         setMovies(data.response.Search.map(el => {
@@ -40,9 +39,7 @@ function Search() {
                         }));
                         setPages(Math.round(data.response.totalResults / 10));
                         setCurrentPage(value);
-                        console.log(data.response)
                     }
-
                 })
                 // Handle error
                 .catch(err => {
@@ -51,32 +48,23 @@ function Search() {
         }
     }
 
-
-
+    // Handle add nominate 
     function nominate(el) {
         el.active = true;
-        // console.log(el)
         if (nominates.length < 5) {
             setNominate([...nominates, el])
             if (nominates.length === 4) {
-                toastr["success"]('Congratylations! You successfully did 5 nominates!')
+                toastr["success"]('Congratylations! You successfully did 5 nominates!');
             }
         } else {
-            toastr["error"]("You've already done 5 nominates")
-            // console.log([...nominates, el])
+            toastr["error"]("You've already done 5 nominates");
         }
-
-
     }
 
-
-
+    // Handle nominate remove 
     function removeNominate(el, index) {
-        // const index = nominates.indexOf(el);
         nominates.splice(index, 1);
-        console.log('nominates==>', nominates)
-        console.log('nominates==>', nominates)
-        setNominate([...nominates])
+        setNominate([...nominates]);
         el.active = false;
     }
 
@@ -86,89 +74,88 @@ function Search() {
             <Container>
                 <Row>
                     <Col>
-                    <br></br>
-                    <h1>The Shoppies</h1>
+                        <br></br>
+                        <h1>The Shoppies</h1>
                     </Col>
                 </Row>
-            
-            
-                <Row>
-                    <Col> 
-            <br></br>
-            <h4>Movie title</h4>
-            <br></br>
-            <input className="search" id="search" type="text" placeholder="Search movie" onChange={e => { setName(e.target.value) }} onKeyPress={event => { if (event.key === "Enter") { search() } }}></input>
-            <button className="button" name="search" onClick={() => { search(1) }}>Search</button>
-            <br></br>
-            </Col>
-                </Row>
-            
 
-                <Row>       
-                    <Col sm={8}> 
-                    <br></br> 
-            <h4>Result for {name.length > 0 ? `"${name}"` :  undefined }</h4>
-            <div>
-                {movies.length === 0 ? "Search and nominate your favorite movie" : movies.map((el, index) => {
-                    return (
-                        <div key={index}>
-                            <br></br>
-                            <div className="center"><span className="title">{el.Title}</span><br></br><img src={el.Poster === 'N/A' ? 'https://upload.wikimedia.org/wikipedia/commons/1/16/No_image_available_450_x_600.svg' : el.Poster}></img>Year of release {el.Year}<br></br><button className="button" id="nominate" disabled={el.active} onClick={e => { nominate(el) }}>Nominate</button><br></br></div>
+
+                <Row>
+                    <Col>
+                        <br></br>
+                        <h4>Movie title</h4>
+                        <br></br>
+                        <input className="search" id="search" type="text" placeholder="Search movie" onChange={e => { setName(e.target.value) }} onKeyPress={event => { if (event.key === "Enter") { search(1) } }}></input>
+                        <button className="button" name="search" onClick={() => { search(1) }}>Search</button>
+                        <br></br>
+                    </Col>
+                </Row>
+
+
+                <Row>
+                    <Col sm={8}>
+                        <br></br>
+                        <h4>Result for {name.length > 0 ? `"${name}"` : ""}</h4>
+                        <div>
+                            {movies.length === 0 ? "Search and nominate your favorite movie" : movies.map((el, index) => {
+                                return (
+                                    <div key={index}>
+                                        <br></br>
+                                        <div className="center"><span className="title">{el.Title}</span><br></br><img src={el.Poster === 'N/A' ? 'https://upload.wikimedia.org/wikipedia/commons/1/16/No_image_available_450_x_600.svg' : el.Poster}></img>Year of release {el.Year}<br></br><button className="button" id="nominate" disabled={el.active} onClick={e => { nominate(el) }}>Nominate</button><br></br></div>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    )
-                })}
-            </div>
-            <br></br>
-            <div className="center"> 
-                        { (()=> {
-                            if(currentPage - 2 >= 1)
-                            return (
-                                <a href="#"  onClick={() => { search(currentPage-2) }}>{currentPage-2}</a> 
-                            )
-                        })()}
-                        { (()=> {
-                            if(currentPage - 1 >= 1)
-                            return (
-                                <a href="#"  onClick={() => { search(currentPage-1) }}>{currentPage-1}</a> 
-                            )
-                        })()}
-                          { (()=> {
-                            if(movies.length >= 1)
-                            return (
-                                // <a href="#" style={{ color: 'red' }}  onClick={() => { search(currentPage) }}>{currentPage}</a> 
-                                <a href="#" onClick={() => { search(currentPage) }}>{currentPage}</a>
-                            )
-                        })()}                        
-                        { (()=> {
-                            if(currentPage + 1 <= pages)
-                            return (
-                                <a href="#"  onClick={() => { search(currentPage+1) }}>{currentPage+1}</a> 
-                            )
-                        })()}
-                         { (()=> {
-                            if(currentPage + 2 <= pages)
-                            return (
-                                <a href="#"  onClick={() => { search(currentPage+2) }}>{currentPage+2}</a> 
-                            )
-                        })()}
-            </div>
-            
-            </Col>
-                    <Col sm={4}> 
-                    <br></br>         
-            <h4>Nominations</h4>
-            <br></br>
-            <ul>
-                {nominates.map((el, index) => {
-                    return (
-                        <li key={index}>
-                            <div>{el.Title} ({el.Year}) <button className="button" id="remove-nominate" onClick={e => { removeNominate(el, index) }}>Remove</button></div>
-                            <br></br>
-                        </li>
-                    )
-                })}
-            </ul>
-            </Col>
+                        <br></br>
+                        <div className="center">
+                            {(() => {
+                                if (currentPage - 2 >= 1)
+                                    return (
+                                        <a href="#" onClick={() => { search(currentPage - 2) }}>{currentPage - 2}</a>
+                                    )
+                            })()}
+                            {(() => {
+                                if (currentPage - 1 >= 1)
+                                    return (
+                                        <a href="#" onClick={() => { search(currentPage - 1) }}>{currentPage - 1}</a>
+                                    )
+                            })()}
+                            {(() => {
+                                if (movies.length >= 1)
+                                    return (
+                                        <a href="#" className="focus" onClick={() => { search(currentPage) }}>{currentPage}</a>
+                                    )
+                            })()}
+                            {(() => {
+                                if (currentPage + 1 <= pages)
+                                    return (
+                                        <a href="#" onClick={() => { search(currentPage + 1) }}>{currentPage + 1}</a>
+                                    )
+                            })()}
+                            {(() => {
+                                if (currentPage + 2 <= pages)
+                                    return (
+                                        <a href="#" onClick={() => { search(currentPage + 2) }}>{currentPage + 2}</a>
+                                    )
+                            })()}
+                        </div>
+
+                    </Col>
+                    <Col sm={4}>
+                        <br></br>
+                        <h4>Nominations</h4>
+                        <br></br>
+                        <ul>
+                            {nominates.map((el, index) => {
+                                return (
+                                    <li key={index}>
+                                        <div>{el.Title} ({el.Year}) <button className="button" id="remove-nominate" onClick={e => { removeNominate(el, index) }}>Remove</button></div>
+                                        <br></br>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </Col>
                 </Row>
             </Container>
         </div>
